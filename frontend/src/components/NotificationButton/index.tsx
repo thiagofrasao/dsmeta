876cc06f195b37d5346c08a778a52fc67ff098b9
Thiago Frasão <thiagofrasao@gmail.com>
2022-09-11 13:51:24 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificayionButton() {
     return (
@@ -11,4 +11,4 @@ function NotificayionButton() {
     )
 }
 
-export default NotificayionButton
+export default NotificayionButton;
